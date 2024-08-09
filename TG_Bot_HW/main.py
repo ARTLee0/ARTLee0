6899347ccc23f2c848f5b43394bde4bfe8363897
Artem Lepestov <150675028+ARTLee0@users.noreply.github.com>
2024-08-09 15:06:54 +0300

    Рабочий вариант

---
+++ TG_Bot_HW/main.py
@@ -0,0 +1,15 @@
+from database import create_table
+from database import dp
+from database import bot
+from database import asyncio
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+
+    # Запускаем создание таблицы базы данных
+    await create_table()
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
